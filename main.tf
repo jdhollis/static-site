caea971035d59575197a7df7dbf187e9b42f7070
J.D. Hollis <jd@densityofspace.com>
2020-11-27 13:59:47 -0500

    Update certificate validation handling for the new provider

--- main.tf
+++ main.tf
@@ -14,16 +14,24 @@ resource "aws_acm_certificate" "site" {
 }
 
 resource "aws_route53_record" "site_cert_validation" {
-  name    = aws_acm_certificate.site.domain_validation_options.0.resource_record_name
-  records = [aws_acm_certificate.site.domain_validation_options.0.resource_record_value]
+  for_each = {
+    for dvo in aws_acm_certificate.site.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
   ttl     = 60
-  type    = aws_acm_certificate.site.domain_validation_options.0.resource_record_type
+  type    = each.value.type
   zone_id = data.aws_route53_zone.site.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.site.arn
-  validation_record_fqdns = [aws_route53_record.site_cert_validation.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.site_cert_validation : record.fqdn]
 }
 
 resource "aws_s3_bucket" "site" {
